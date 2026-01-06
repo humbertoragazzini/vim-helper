9bfaf1c8b00a41eb1c83ef14781b8bb48c446ebb
Humberto Ragazzini <79940052+humbertoragazzini@users.noreply.github.com>
2026-01-06 16:42:36 +0000

    added path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vim-helper/',
   plugins: [react(), tailwindcss()],
+  base: '/vim-helper/',
 })
